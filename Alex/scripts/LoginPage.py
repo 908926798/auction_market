a730fd06d76eee62ba334527c69c817f73a9e63a
陈星霖 <908926798@qq.com>
2018-09-15 08:50:57 +0800

    呕心沥血时序图1.0

--- Alex/scripts/LoginPage.py
+++ Alex/scripts/LoginPage.py
@@ -1,20 +1,19 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.uic import loadUi
+import sys
 
-class LoginPage(QWidget):
+class LoginPage(QDialog):
     close_signal = pyqtSignal()
-    def __init__(self, rP,parent=None):
+    def __init__(self, mc,parent=None):
         # super这个用法是调用父类的构造函数
         # parent=None表示默认没有父Widget，如果指定父亲Widget，则调用之
         super(LoginPage, self).__init__(parent)
-        loadUi('UI/test1.ui',self)
-        self.registerPage = rP
-        self.pushButton.clicked.connect(self.login)
-
-    def closeEvent(self, event):
-        self.close_signal.emit()
-        self.close()
+        loadUi('UI/loginPage.ui',self)
+        self.btn_login.clicked.connect(self.login)
+        self.btn_toRegister.clicked.connect(self.toRegister)
+        self.mainController = mc
+        self.opened = False
 
     def login(self):
         ok = True
@@ -22,4 +21,18 @@ class LoginPage(QWidget):
         #调用数据库
         ##########################
         if ok:
-            self.registerPage.handle_click()
+            self.mainController.username = 'cxl'
+            self.mainController.nextPage = 'mainPage'
+            self.close()
+
+    def toRegister(self):
+        self.mainController.nextPage = 'registerPage'
+        self.close()
+
+    def closeEvent(self, event):
+        if not self.mainController.nextPage:
+            sys.exit()
+
+    def run(self):
+        self.mainController.nextPage = None
+        self.exec_()
--- Alex/scripts/client.py
+++ Alex/scripts/client.py
@@ -3,17 +3,28 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import LoginPage
 import RegisterPage
+import MainPage
+
+class MainController():
+    def __init__(self):
+        self.username = None
+        self.mainServerIP = None
+        self.pages = {}
+        self.nextPage = None
+        self.exit = False
 
 if __name__ == "__main__":
     App = QApplication(sys.argv)
 
+    mc = MainController()
     #登录和注册
-    registerPage = RegisterPage.RegisterPage()
+    mc.pages['registerPage'] = RegisterPage.RegisterPage(mc)
+    mc.pages['loginPage'] = LoginPage.LoginPage(mc)
+    mc.pages['mainPage'] = MainPage.MainPage(mc)
 
-    loginPage = LoginPage.LoginPage(registerPage)
+    mc.nextPage = 'mainPage'
 
-    loginPage.pushButton.clicked.connect(loginPage.hide)
-    loginPage.close_signal.connect(loginPage.close)
+    while(mc.nextPage):
+        mc.pages[mc.nextPage].run()
 
-    loginPage.show()
     sys.exit(App.exec_())
