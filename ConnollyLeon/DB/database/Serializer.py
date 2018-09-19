20fb8861e3bbaa149d1ca9febe8b81c2d44da46a
connolly <connollyleon@foxmail.com>
2018-09-19 17:00:05 +0800

    G_number

--- ConnollyLeon/DB/database/Serializer.py
+++ ConnollyLeon/DB/database/Serializer.py
@@ -2,18 +2,21 @@ from rest_framework import serializers
 
 
 class GoodsReviewSerializer(serializers.Serializer):
+    G_number = serializers.IntegerField()
     goods_name = serializers.CharField(max_length=30)
     seller_name = serializers.CharField(read_only=True, max_length=30)
     minimum_price = serializers.IntegerField(required=False)
 
 
 class GoodsInSerializer(serializers.Serializer):
+    G_number = serializers.IntegerField()
     goods_name = serializers.CharField(max_length=30)
     lastbid_username = serializers.CharField(max_length=20)
     seller_name = serializers.CharField(read_only=True, max_length=30)
     lastprice = serializers.IntegerField()
 
 class GoodsEndSerializer(serializers.Serializer):
+    G_number = serializers.IntegerField()
     goods_name = serializers.CharField(max_length=30)
     lastbid_username = serializers.CharField(max_length=20)
     lastbid_time = serializers.DateTimeField()
